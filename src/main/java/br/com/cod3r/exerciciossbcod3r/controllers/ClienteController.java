ea7491306c748f5b87b42e046d91f6f343bc01ab
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-11-20 11:28:46 -0300

    Métodos HTTP.

--- src/main/java/br/com/cod3r/exerciciossbcod3r/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossbcod3r/controllers/ClienteController.java
@@ -1,14 +1,24 @@
 package br.com.cod3r.exerciciossbcod3r.controllers;
 
 import br.com.cod3r.exerciciossbcod3r.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @GetMapping(path = "/clientes/qualquer")
+    @GetMapping(path = "/qualquer")
     public Cliente obterClietne() {
         return new Cliente(35, "Wendel", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Melina", "123.456.789-00");
+    }
+
+    @GetMapping
+    public Cliente obterclientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+        return new Cliente(id, "Heitor", "987.654.321-00");
     }
 }
