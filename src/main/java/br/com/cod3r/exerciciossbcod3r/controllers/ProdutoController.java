c4e59eaab2a65e580074e6a3e82a8210c370fb3f
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-11-20 18:35:02 -0300

    Aplicando validações simples.

--- src/main/java/br/com/cod3r/exerciciossbcod3r/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossbcod3r/controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import br.com.cod3r.exerciciossbcod3r.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
@@ -13,7 +15,7 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public @ResponseBody Produto novoProduto(Produto produto) {
+    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
--- src/main/java/br/com/cod3r/exerciciossbcod3r/model/entities/Produto.java
+++ src/main/java/br/com/cod3r/exerciciossbcod3r/model/entities/Produto.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor
 @Entity
@@ -20,14 +23,18 @@ public class Produto {
 
     @Getter
     @Setter
+    @NotBlank
     private String nome;
 
     @Getter
     @Setter
+    @Min(0)
     private double preco;
 
     @Getter
     @Setter
+    @Min(0)
+    @Max(1)
     private double desconto;
 
     public Produto(String nome, double preco, double desconto) {
